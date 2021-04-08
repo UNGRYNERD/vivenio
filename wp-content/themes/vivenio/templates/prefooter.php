eef181680fdda18c5ee3e76e8ba6305a560169b9
Daniel Aguilar <dacmail@gmail.com>
2021-04-08 17:29:44 +0200

    remove renta corp link in prefooter

--- wp-content/themes/vivenio/templates/prefooter.php
+++ wp-content/themes/vivenio/templates/prefooter.php
@@ -1,7 +1,6 @@
 <section class="prefooter">
   <div class="prefooter__text">
     <?php the_field('footer_text', get_option('page_on_front')); ?>
-    <p class="renta"><a href="http://rentacorporacion.com/es/" target="_blank"><?php esc_html_e('Gestionado por Renta Corporación', 'vivenio'); ?></a></p>
   </div>
   <div class="prefooter__contact">
     <h2 class="prefooter__title"><?php esc_html_e('Estaremos encantados de ayudarte.', 'vivenio'); ?></h2>
